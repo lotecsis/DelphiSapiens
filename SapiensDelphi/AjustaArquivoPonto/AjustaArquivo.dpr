b893cc9ce29ae5e0f275ed7cac02a4515344e9cb
mvDelphi2010_Lider <douglas.double@gmail.com>
2018-01-09 10:40:08 -0200

    Ajusta arquivo ponto

---
+++ SapiensDelphi/AjustaArquivoPonto/AjustaArquivo.dpr
@@ -0,0 +1,14 @@
+program AjustaArquivo;
+
+uses
+  Forms,
+  UPrincipal in 'UPrincipal.pas' {FPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFPrincipal, FPrincipal);
+  Application.Run;
+end.
